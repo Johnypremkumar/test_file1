47ab17bee1f8e99bdf2ab49fbb2a9000740f413b
Premkumar, Johny <Johny.Premkumar@xpo.com>
2020-06-11 11:47:09 +0530

    first commit

---
+++ src/test/java/com/example/test1/demo/test/file1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test1.demo.test.file1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
